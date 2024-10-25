9eb9896d4fcaa11f5e6f5a2fd1b855727a1c59d1
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2024-10-24 19:49:15 -0600

    added basic app with mongodb connection

---
+++ util/mongodb.js
@@ -0,0 +1,37 @@
+import { MongoClient } form 'mongodb';
+
+let uri = process.env.MONGODB_URI;
+let dbName = process.env.MONGODB_DB;
+
+let cachedClient = null;
+let chachedDB = null;
+
+if (!uri) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable'
+  );
+}
+
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGODB_DB environment variable'
+  );
+}
+
+export async function connectToDB() {
+  if (cachedClient && cachedDB) {
+    return { client: cachedClient, db: cachedDB };
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifyiedTopology: true,
+  });
+
+  const db = await client.db(dbName);
+
+  cachedClient = client;
+  cachedDB = db;
+
+  return { client, db };
+};
