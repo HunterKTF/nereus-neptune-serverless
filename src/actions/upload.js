a9a7de0a61a363343bb2c365af18edddd108f69f
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2024-10-26 11:43:18 -0600

    added file reader to get a json data book

---
+++ src/actions/upload.js
@@ -0,0 +1,86 @@
+'use server'
+
+import XLSX from "xlsx";
+
+export async function ParseData(file) {
+    // Read sheet file from buffer
+    const workbook = XLSX.read(file);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    // Get doc general info
+    const dates = worksheet.A3.v.replace("del ", "").split(" al ");
+    const documentInfo = {
+        init_date: dates[0],
+        end_date: dates[1],
+    }
+
+    // Decode the file length for max_rows
+    const rows = XLSX.utils.decode_range(worksheet['!ref']);
+    const max_rows = rows.e.r;
+
+    const dataBook = await TransformData(worksheet, max_rows);
+
+    return dataBook;
+}
+
+export async function TransformData(worksheet, max_rows) {
+    // Declare pointer variables 
+    let account_num, account_name;
+    let breaker, cargos, abonos, date;
+
+    // Declare dynamic analytics data
+    let groupedData = {};
+    let account_index = "000-00-000";
+    let account_data = { year: 0, cargos: 0.0, abonos: 0.0 };
+    let accounts = ['000', '00', '000'];
+    let account_group = 0;
+
+    // Begin main for loop
+    for (let i = 9; i < max_rows; i++) {
+        // Get cell data related to account, account name and date
+        account_num = worksheet[`A${i}`] || { v: ' ' };
+        // account_name = worksheet[`B${i}`] || { v: ' ' };
+
+        // Get cell data related to breakpoint, credit and debit
+        breaker = worksheet[`E${i}`] || { v: ' ' };
+        cargos = worksheet[`F${i}`] || { v: 0 };
+        abonos = worksheet[`G${i}`] || { v: 0 };
+
+        // Look for subaccounts
+        if (account_num.v != ' ' && account_num.v.includes('-')) {
+            // Set new account
+            accounts = account_num.v.split('-');
+            
+            if (accounts[1] !== '00') {
+                account_index = accounts[0] + '-' + accounts[1] + '-000';
+                account_group = parseInt(accounts[0][0] + '00');
+                if (account_group in groupedData) {} 
+                else {
+                    groupedData[account_group] = {};
+                }
+                account_data = { year: 0, cargos: 0.0, abonos: 0.0 };
+            }
+        }
+
+        // Look for data and make the sum
+        if (account_num.v != ' ' && account_num.v.includes('/')) {
+            date = account_num.v.split('/');
+            account_data.year = date[2];
+            account_data.cargos += cargos.v;
+            account_data.abonos += abonos.v;
+        }
+
+        // Save data in dictionary when breakpoint appears
+        if (breaker.v === "Total:" && account_data.year !== 0) {
+            if (account_index in groupedData[account_group]) {
+                groupedData[account_group][account_index].cargos += account_data.cargos;
+                groupedData[account_group][account_index].abonos += account_data.abonos;
+            } else {
+                groupedData[account_group][account_index] = account_data;
+            }
+        }
+    }
+
+    return groupedData;
+}
--- src/app/upload/route.js
+++ src/app/upload/route.js
@@ -1,7 +1,26 @@
 'use server'
 
-import { NextResponse } from 'next/server'
+import { ParseData } from "@/actions/upload";
 
-export async function GET(req) {
-  return NextResponse.json({ message: 'Hello world' }, { status: 200 })
+// POST method to upload Contpaqi file
+export async function POST(request) {
+  const formData = await request.formData();  // Get form-data from body
+
+  // Extract data
+  const clientId = formData.get('clientId');
+  const file = formData.get('file');
+
+  // Check if file is uploaded correctly
+  if (!file) {
+    console.log('No file was uploaded');
+    return Response.json({ error: "No file encountered in form-data" }, { status: 404 });
+  } else {
+    // Get file data from buffer and parse data
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    // Parse data into a dictionary
+    const dataBook = await ParseData(buffer);
+
+    return Response.json({ dataBook }, { status: 200 });
+  }
 }
