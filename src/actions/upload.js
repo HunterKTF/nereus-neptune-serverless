52f0fcd996ccfe8b98d91ed21a5720b5b079edca
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2024-11-06 17:03:00 -0600

    added monthly format to cargos and abonos metrics

--- src/actions/upload.js
+++ src/actions/upload.js
@@ -33,8 +33,14 @@ export async function TransformData(worksheet, max_rows) {
 
     // Declare dynamic analytics data
     let groupedData = {};
-    let account_index = "000-00-000";
-    let account_data = { year: 0, cargos: 0.0, abonos: 0.0 };
+    let sub_account = "000-00-000";
+    let account_data = {};
+    let data_cargos = {};
+    let data_abonos = {};
+    const data_reset = {
+        ene: 0.0, feb: 0.0, mar: 0.0, abr: 0.0, may: 0.0, jun: 0.0,
+        jul: 0.0, ago: 0.0, sep: 0.0, oct: 0.0, nov: 0.0, dic: 0.0, sum: 0.0
+    };
     let accounts = ['000', '00', '000'];
     let account_group = 0;
 
@@ -54,32 +60,58 @@ export async function TransformData(worksheet, max_rows) {
             // Set new account
             accounts = account_num.v.split('-');
 
+            // Filter for sub- or micro-accounts
             if (accounts[1] !== '00') {
-                account_index = accounts[0] + '-' + accounts[1] + '-000';
+                sub_account = accounts[0] + '-' + accounts[1] + '-000';
                 account_group = parseInt(accounts[0][0] + '00');
                 if (account_group in groupedData) { }
                 else {
                     groupedData[account_group] = {};
                 }
-                account_data = { year: 0, cargos: 0.0, abonos: 0.0 };
+                account_data = { year: 0, cargos: {}, abonos: {} };
+                data_cargos = {
+                    ene: 0.0, feb: 0.0, mar: 0.0, abr: 0.0, may: 0.0, jun: 0.0,
+                    jul: 0.0, ago: 0.0, sep: 0.0, oct: 0.0, nov: 0.0, dic: 0.0, sum: 0.0
+                };
+                data_abonos = {
+                    ene: 0.0, feb: 0.0, mar: 0.0, abr: 0.0, may: 0.0, jun: 0.0,
+                    jul: 0.0, ago: 0.0, sep: 0.0, oct: 0.0, nov: 0.0, dic: 0.0, sum: 0.0
+                };
             }
         }
 
         // Look for data and make the sum
         if (account_num.v != ' ' && account_num.v.includes('/')) {
+            // Split dates found into an array
             date = account_num.v.split('/');
-            account_data.year = date[2];
-            account_data.cargos += cargos.v;
-            account_data.abonos += abonos.v;
+
+            // Store dates into variables
+            let year = date[2];
+            let month = date[1].toLowerCase();
+
+            // Set the current data year
+            account_data.year = year;
+
+            // Set the data from 'cargos' in current month and total sum
+            data_cargos[month] += cargos.v;
+            data_cargos['sum'] += cargos.v;
+
+            // Set the data from 'abonos' in current month and total sum
+            data_abonos[month] += abonos.v;
+            data_abonos['sum'] += abonos.v;
         }
 
         // Save data in dictionary when breakpoint appears
         if (breaker.v === "Total:" && account_data.year !== 0) {
-            if (account_index in groupedData[account_group]) {
-                groupedData[account_group][account_index].cargos += account_data.cargos;
-                groupedData[account_group][account_index].abonos += account_data.abonos;
+            if (sub_account in groupedData[account_group]) {
+                for (let months in data_reset) {
+                    groupedData[account_group][sub_account].cargos[months] += data_cargos[months];
+                    groupedData[account_group][sub_account].abonos[months] += data_abonos[months];
+                }
             } else {
-                groupedData[account_group][account_index] = account_data;
+                account_data.cargos = data_cargos;
+                account_data.abonos = data_abonos;
+                groupedData[account_group][sub_account] = account_data;
             }
         }
     }
@@ -109,11 +141,14 @@ export async function ParseMetrics(dataBook, clientId) {
             // Pass data to local function and extract temporal metric value
             tmp_metric = await sumByGroup(group, dataBook, sums, takes);
 
-            // Test temporal metric output
-            // console.log(tmp_metric);
-        }
-        else {
-            tmp_metric = sumByReq(metrics_ref[line].requires, dataBook);
+        } else {
+            // Decalre requirements constraints
+            let sums = metrics_ref[line].sums;
+            let takes = metrics_ref[line].takes;
+            let req_list = metrics_ref[line].requires;
+
+            // Pass data to local function and extract temporal metric value
+            tmp_metric = sumByReq(group, dataBook, sums, takes);
 
             // Test temporal metrics by requirement
             // console.log(tmp_metric);
@@ -137,21 +172,60 @@ export async function ParseMetrics(dataBook, clientId) {
     return db_metrics;
 }
 
-async function sumByGroup(group, db, sums, takes) {
-    const accounts = db[group];
-    let group_sum = 0;
+async function sumByGroup(group, dataBook, sums, takes) {
+    const accounts = dataBook[group];
+    let group_ene = 0.0;
+    let group_feb = 0.0;
+    let group_mar = 0.0;
+    let group_abr = 0.0;
+    let group_may = 0.0;
+    let group_jun = 0.0;
+    let group_jul = 0.0;
+    let group_ago = 0.0;
+    let group_sep = 0.0;
+    let group_oct = 0.0;
+    let group_nov = 0.0;
+    let group_dic = 0.0;
+    let group_sum = 0.0;
     let year = '';
 
     // Iterate over group elements
-    for (let subaccount in accounts){
-        group_sum += accounts[subaccount][sums] - accounts[subaccount][takes]
+    for (let subaccount in accounts) {
+        group_ene += accounts[subaccount][sums].ene - accounts[subaccount][takes].ene;
+        group_feb += accounts[subaccount][sums].feb - accounts[subaccount][takes].feb;
+        group_mar += accounts[subaccount][sums].mar - accounts[subaccount][takes].mar;
+        group_abr += accounts[subaccount][sums].abr - accounts[subaccount][takes].abr;
+        group_may += accounts[subaccount][sums].may - accounts[subaccount][takes].may;
+        group_jun += accounts[subaccount][sums].jun - accounts[subaccount][takes].jun;
+        group_jul += accounts[subaccount][sums].jul - accounts[subaccount][takes].jul;
+        group_ago += accounts[subaccount][sums].ago - accounts[subaccount][takes].ago;
+        group_sep += accounts[subaccount][sums].sep - accounts[subaccount][takes].sep;
+        group_oct += accounts[subaccount][sums].oct - accounts[subaccount][takes].oct;
+        group_nov += accounts[subaccount][sums].nov - accounts[subaccount][takes].nov;
+        group_dic += accounts[subaccount][sums].dic - accounts[subaccount][takes].dic;
+        group_sum += accounts[subaccount][sums].sum - accounts[subaccount][takes].sum;
         year = accounts[subaccount]['year'];
     }
 
-    return { sum: group_sum, year: year };
+    return { 
+        ene: group_ene.toFixed(2),
+        feb: group_feb.toFixed(2),
+        mar: group_mar.toFixed(2),
+        abr: group_abr.toFixed(2),
+        may: group_may.toFixed(2),
+        jun: group_jun.toFixed(2),
+        jul: group_jul.toFixed(2),
+        ago: group_ago.toFixed(2),
+        sep: group_sep.toFixed(2),
+        oct: group_oct.toFixed(2),
+        nov: group_nov.toFixed(2),
+        dic: group_dic.toFixed(2),
+        sum: group_sum.toFixed(2), 
+        year: year 
+    };
 }
 
-async function sumByReq() {
+async function sumByReq(group, dataBook, sums, takes) {
 
     return {};
 }
