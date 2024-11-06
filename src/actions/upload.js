6bd8d471e5a23e0f0bb8ddf4bb60c04c7a0cf6aa
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2024-11-05 18:33:01 -0600

    finished part 1 of metrics algorithm with local MongoDB connection

--- src/actions/upload.js
+++ src/actions/upload.js
@@ -1,6 +1,7 @@
 'use server'
 
 import XLSX from "xlsx";
+import reference from './reference.json';
 
 export async function ParseData(file) {
     // Read sheet file from buffer
@@ -20,7 +21,7 @@ export async function ParseData(file) {
     const max_rows = rows.e.r;
 
     const dataBook = await TransformData(worksheet, max_rows);
-    console.log(dataBook);
+    // console.log(dataBook);
 
     return dataBook;
 }
@@ -52,11 +53,11 @@ export async function TransformData(worksheet, max_rows) {
         if (account_num.v != ' ' && account_num.v.includes('-')) {
             // Set new account
             accounts = account_num.v.split('-');
-            
+
             if (accounts[1] !== '00') {
                 account_index = accounts[0] + '-' + accounts[1] + '-000';
                 account_group = parseInt(accounts[0][0] + '00');
-                if (account_group in groupedData) {} 
+                if (account_group in groupedData) { }
                 else {
                     groupedData[account_group] = {};
                 }
@@ -84,4 +85,73 @@ export async function TransformData(worksheet, max_rows) {
     }
 
     return groupedData;
+}
+
+export async function ParseMetrics(dataBook, clientId) {
+    // Declare global variables
+    let db_metrics = [];
+
+    // Get the metrics conditions from json file
+    const metrics_ref = reference.metrics;
+
+    // Iterate over the metrics condition generation
+    for (let line in metrics_ref) {
+        // Temporal metric will act as row to append
+        let tmp_metric = {};
+
+        // Check for metric condition as group or requirements
+        if (metrics_ref[line].group) {
+            // Decalre group constraints
+            let sums = metrics_ref[line].sums;
+            let takes = metrics_ref[line].takes;
+            let group = metrics_ref[line].requires;
+
+            // Pass data to local function and extract temporal metric value
+            tmp_metric = await sumByGroup(group, dataBook, sums, takes);
+
+            // Test temporal metric output
+            // console.log(tmp_metric);
+        }
+        else {
+            tmp_metric = sumByReq(metrics_ref[line].requires, dataBook);
+
+            // Test temporal metrics by requirement
+            // console.log(tmp_metric);
+        }
+
+        // Add parameters to temporal metric variable
+        tmp_metric.client_id = clientId;
+        tmp_metric.name = metrics_ref[line].name;
+
+        // Check if data is available
+        if (tmp_metric.year !== '') {
+            // Add metric to dabatase metrics
+            db_metrics.push(tmp_metric);
+        }
+    }
+
+    // Test return of completed database metrics
+    console.log(db_metrics);
+
+    // Return the ordered metrics
+    return db_metrics;
+}
+
+async function sumByGroup(group, db, sums, takes) {
+    const accounts = db[group];
+    let group_sum = 0;
+    let year = '';
+
+    // Iterate over group elements
+    for (let subaccount in accounts){
+        group_sum += accounts[subaccount][sums] - accounts[subaccount][takes]
+        year = accounts[subaccount]['year'];
+    }
+
+    return { sum: group_sum, year: year };
+}
+
+async function sumByReq() {
+
+    return {};
 }
--- src/app/upload/route.js
+++ src/app/upload/route.js
@@ -1,6 +1,7 @@
 'use server'
 
-import { ParseData } from "@/actions/upload";
+import { ParseData, ParseMetrics } from "@/actions/upload";
+import { connectToDB } from "@/utils/mongodb";
 
 // POST method to upload Contpaqi file
 export async function POST(request) {
@@ -21,6 +22,35 @@ export async function POST(request) {
     // Parse data into a dictionary
     const dataBook = await ParseData(buffer);
 
-    return Response.json({ dataBook }, { status: 200 });
+    // Parse dataBook into metrics to post to MongoDB
+    const metrics = await ParseMetrics(dataBook, clientId);
+
+    // Upload transformed metrics into MongoDB
+    try {
+      // Begin MongoDB connection to metrics collection
+      const { client, db } = await connectToDB();
+      const myColl = db.collection(process.env.MONGODB_COLL);
+
+      // Upload data
+      const result = await myColl.insertMany(metrics);
+
+      // Close MongoDB connection
+      client.close();
+
+      // Return the message of successfull upload
+      return Response.json({ message: `${result.insertedCount} documents were inserted.` });
+    } catch (e) {
+      // Print error message and correctly uploaded docs
+      console.log(`A MongoBulkWriteException occurred, but there are successfully processed documents.`);
+
+      // Iterate over ids generated
+      let ids = e.result.result.insertedIds;
+      for (let id of Object.values(ids)) {
+        console.log(`Processed a document with id ${id._id}`);
+      }
+
+      // Return the message of successfull upload
+      return Response.json({ message: `Number of documents inserted: ${e.result.result.nInserted}` });
+    }
   }
 }
