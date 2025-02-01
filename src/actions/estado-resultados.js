35b9a9bde040a28af12f616756867337488ee218
Jorge Ricardo Hernández Sabino <40583956+HunterKTF@users.noreply.github.com>
2025-01-31 22:53:58 -0600

    added support to read balance general and estado de resultados

---
+++ src/actions/estado-resultados.js
@@ -0,0 +1,52 @@
+'use server'
+
+import XLSX from "xlsx";
+import kpiBlueprints from './kpiBlueprints.json';
+
+
+export async function ParseData(file) {
+    // Read file from buffered data
+    const workbook = XLSX.read(file);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    // Get document information
+    const client_name = worksheet.C1.v;
+    const dates = worksheet.A2.v.replace("Estado de Resultados del  ", "").split("  al  ");
+
+    const doc_info = {
+        client: client_name,
+        date_init: dates[0],
+        date_end: dates[1]
+    }
+    console.log(doc_info);
+
+    const docMetrics = {
+        ingresos: {
+            ingresos: worksheet.B8.v,
+            otros_ingresos: worksheet.B9.v,
+            total: worksheet.B11.v,
+        },
+        egresos: {
+            costos: {
+                compras: worksheet.B16.v,
+                total: worksheet.B18.v,
+            },
+            gastos: {
+                gastos_gen: worksheet.B21.v,
+                gastos_admin: worksheet.B22.v,
+                depr_cont: worksheet.B23.v,
+                total: worksheet.B25.v,
+            },
+            resul_integ_fin: {
+                gastos_fin: worksheet.B28.v,
+                prod_fin: worksheet.B29.v,
+                total: worksheet.B31.v,
+            },
+            total: worksheet.B34.v,
+        },
+        utilidad_perdida: worksheet.B38.v,
+    }
+
+    return docMetrics;
+}
---
+++ src/app/upload/balance-general/route.js
@@ -0,0 +1,31 @@
+'use server'
+
+import { ParseData } from "@/actions/balance-general";
+import { connectToDB } from "@/utils/mongodb";
+
+
+// TODO: Function to upload data to MongoDB
+
+// POST method to upload contpaqi file
+export async function POST (request) {
+    try {
+        const formData = await request.formData();
+
+        const clientId = formData.get('clientId');
+        const file = formData.get('file');
+
+        if (!file) {
+            console.log('No file was uploaded');
+            return Response.json({ message: "No file was found in request", status: 404 });
+        } else {
+            const buffer = Buffer.from(await file.arrayBuffer());
+
+            const dataBook = await ParseData(buffer);
+
+            return Response.json({ message: "Balance General uploaded correctly", data: dataBook, status: 201 })
+        }
+    } catch (e) {
+        console.log(e);
+        return Response.json({ message: "Could not upload Balance General", status: 400 })
+    }
+}
