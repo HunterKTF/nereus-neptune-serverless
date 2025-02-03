e1affaef4eadec70d0ab9eb88c686b06ff2500b0
Jorge R. Hernández Sabino <jorgehernandez336@gmail.com>
2025-02-03 15:40:41 -0600

    added mongo connection to metrics and kpis

--- src/app/upload/estados-financieros/route.js
+++ src/app/upload/estados-financieros/route.js
@@ -7,7 +7,32 @@ import { ParseIndicators } from "@/actions/upload";
 import { connectToDB } from "@/utils/mongodb";
 
 
-// TODO: Upload to MongoDB
+// Upload to MongoDB
+// Function to upload data to MongoDB
+async function uploadToMongo(metrics, kpis) {
+    try {
+      // Begin MongoDB connection to metrics collection
+      const { client, db } = await connectToDB();
+      const metricsColl = db.collection(process.env.MONGODB_COLL_MET);
+      const kpiColl = db.collection(process.env.MONGODB_COLL_KPI);
+  
+      // Upload metrics
+      const metricsRes = await metricsColl.insertMany(metrics);
+      console.log(`${metricsRes.insertedCount} documents were inserted.`);
+      
+      // Upload kpis
+      const kpiRes = await kpiColl.insertMany(kpis);
+      console.log(`${kpiRes.insertedCount} documents were inserted.`);
+  
+      return { message: `${metricsRes.insertedCount} metrics were inserted. ${kpiRes.insertedCount} KPIs were inserted`, status: 200 };
+    } catch (e) {
+      // Print error message and correctly uploaded docs
+      console.log(`A MongoBulkWriteException occurred, but there are successfully processed documents.`);
+      console.log(e);
+  
+      return { message: "Error uploading documents", status: 500 };
+    }
+  }
 
 
 // POST method to upload 2 contpaqi files
@@ -53,7 +78,10 @@ export async function POST (request) {
 
             let kpis = await ParseIndicators(metrics_idx);
 
-            return Response.json({ message: message, status: 200, year: year, clientId: clientId, dict: kpis });
+            // Upload transformed metrics into MongoDB
+            const response = await uploadToMongo(metrics, kpis);
+
+            return Response.json(response);
         };
     } catch (e) {
         console.log(e);
